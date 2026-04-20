79ca101a8465098b6882f2e0377a50c26a7f1347
jaskaran singh <jaskaransingh@ucla.edu>
2026-04-19 21:08:35 -0700

    Initial Commit

---
+++ Tones/TonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TonesApp.swift
+//  Tones
+//
+//  Created by Jaskaran Singh on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct TonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
